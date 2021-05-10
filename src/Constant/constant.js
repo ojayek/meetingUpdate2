3e8b4ceb179ab0a1a18c1854ba617becc934a544
ojayek <ghoreyshi@gmail.com>
2021-05-10 10:05:20 +0330

    secound commit

--- src/Constant/constant.js
+++ src/Constant/constant.js
@@ -1,4 +1,9 @@
-export const SERVER_URL = 'http://localhost:58148/api/';
+//export const SERVER_URL = 'http://localhost:58148/api/';
+
+
+//ست شده در لوکال ای ای اس سیستم پورت 89
+
+export const SERVER_URL = 'http://localhost:89/api/';
 //export const SERVER_URL = 'http://backup-db/MailAndContactApi/api/';
 //export const SERVER_URL = "http://localhost:80/ece/api/";
 ///49580
